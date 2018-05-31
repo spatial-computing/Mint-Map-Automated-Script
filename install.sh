1d171b949ad3553ef2e2e0302b3b3eab5d7adebf
Libo Liu <liboliu@usc.edu>
2018-05-31 12:39:45 -0700

    manpage

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+# copy to /usr/local/share/mintcast
 sudo mkdir -p /usr/local/share/mintcast
 sudo cp -r ./bin /usr/local/share/mintcast/
 sudo cp -r ./lib /usr/local/share/mintcast/
 sudo cp -r ./python /usr/local/share/mintcast/
 
+# link mintcast
 sudo mkdir -p /usr/local/bin
 sudo ln -fs /usr/local/share/mintcast/bin/mintcast.sh /usr/local/bin/mintcast
 sudo chmod +x /usr/local/bin/mintcast
+
+# create man page for mintcast
 
 CHECK_PATH="$(echo $PATH | grep -E ':*/usr/local/bin:*' | wc -l | tr  -d ' ')"
 if [[ $CHECK_PATH = "0" ]]; then
