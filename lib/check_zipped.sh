32fc467a3a0d667edf8a367f6cd3d2a09ddd93ca
advaccaro <avaccaro@usc.edu>
2018-05-30 18:06:34 -0700

    Fix catastrophic typo in check_zipped.sh

--- lib/check_zipped.sh
+++ lib/check_zipped.sh
@@ -2,15 +2,15 @@
 set -e
 
 ### check_zipped.sh
-# Checks to see if a directory of tiled  files is zipped and needs to be 
+# Checks to see if a directory of tiled files is zipped and needs to be 
 # unzipped.  Unzips the files if necessary.
 ### Inputs: 
 # Directory of TIFF tile files
 ### Outputs: 
 # Unzips files into directory (if necessary)
 ### Procedure:
-# -Check if there are TIFF files in the directory aleady
-# -If not, unzips zip files
+# - Check if there are TIFF files in the directory aleady
+# - If not, unzips zip files
 
 # Parse arguments:
 export INPUT_DIR=$1 #Directory of tiles to be checked
@@ -20,10 +20,10 @@ export FILE_SUFFIX=$2 #extension (and suffix) of files to merge (e.g. dem.tif)
 export CLEANED_DIR=${INPUT_DIR%/*}
 
 # Check if there are unzipped files in the directory, unzip them if not:
-if ! [ -n "($ls -A $CLEANED_DIR/*$FILE_SUFFIX 2>/dev/null)" ]; then
-	for file in $CLEANED_DIR/*.zip; do
-		unzip -o $file
-	done
-else
+if [ -n "$(ls -A $CLEANED_DIR/*$FILE_SUFFIX 2>/dev/null)" ]; then
 	echo "Files already unzipped."
+else
+	for file in $CLEANED_DIR/*.zip; do
+		unzip -o $file -d $CLEANED_DIR
+	done
 fi
