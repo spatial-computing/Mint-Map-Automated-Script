a1f7a782a7ede0a870772a68ff8c266c59e0da2d
advaccaro <avaccaro@usc.edu>
2018-06-02 11:11:01 -0700

    Change filename to match specifications

---
+++ lib/proc_geojson2mbtiles.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+### geojson2mbtiles.sh
+# Generate zoom levels while converting GeoJSON file to MBTiles.
+### Inputs:
+# 1) Input file, 2) Name of output file, 3) Layer name
+### Outputs:
+# MBTiles file ready for mapping
+### Procedure:
+# - Convert to MBTiles and generate zoom levels with tippecanoe
+
+# Convert GeoJSON to MBTiles:
+convert_geojson() {
+	tippecanoe \
+	-f \
+	-o $2`#Output filename` \
+	-B11 \
+	-Z3 \
+	-z14 \
+	-m8 \
+	-pf \
+	--maximum-tile-bytes=200000 \
+	-s EPSG:3857 \
+	--coalesce-smallest-as-needed \
+	--extend-zooms-if-still-dropping \
+	--layer=$3 `#Layer name`\
+	$1 `#Input filename`
+}
