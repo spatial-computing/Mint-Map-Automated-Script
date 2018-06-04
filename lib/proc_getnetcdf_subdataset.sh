0f82e75c20e82fc0fb3871f0b67778b61683433e
Libo Liu <liboliu@usc.edu>
2018-06-04 15:15:34 -0700

    reversal dir

--- lib/proc_getnetcdf_subdataset.sh
+++ lib/proc_getnetcdf_subdataset.sh
@@ -1,5 +1,7 @@
  #!/usr/bin/env bash
 
+source handle_tiff.sh
+
 handle_netcdf(){
 	SUBDATASET_STRING="$(gdalinfo $DATAFILE_PATH | sed -nE 's/SUBDATASET_.{1,2}_NAME=(.*)/\1/p' | grep -o 'N.*')"
 	IFS=$'\n'
@@ -12,7 +14,7 @@ handle_netcdf(){
 		# gdalwarp -te 22.4 3.4 37.0 23.2 -cutline $MINTCAST_PATH/shp/ss.shp
 		# gdal_translate -a_srs EPSG:3857 -tr 0.01 0.01 "$dataset" "$MINTCAST_PATH/tmp/${name[2]}.tif"
 		# gdalwarp -te 22.4 3.4 37.0 23.2  -dstnodata 255 -cutline $MINTCAST_PATH/shp/ss.shp 
-		exit 0
+		# exit 0
 	done
 	
 }
---
+++ python/macro_traversal/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# argv [./main.py, 'dir','dir structure', 'start', 'end']
+
+import sys
+import os
+import re
+from os.path import isfile, join
+from datetime import datetime, date, time, timedelta
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+def main(argv):
+	_dir = sys.argv[1]
+	_wildcardStructure = sys.argv[2].replace('{year}','%Y').replace('{month}', '%m').replace('{day}','%d')
+	_structure = os.path.dirname(_wildcardStructure)
+	__timeFormat = _structure.replace('/', ' ')
+	_start = datetime.strptime(sys.argv[3], __timeFormat)
+	_end = datetime.strptime(sys.argv[4], __timeFormat)
+	_filetype = os.path.splitext(sys.argv[2])[1]
+
+	files = []
+	_oldpath = ''
+	for single_date in daterange(_start, _end):
+		_folder = single_date.strftime(_structure)
+		_path = _dir + _folder
+		if _oldpath == _path:
+			continue
+
+		for f in os.listdir(_path):
+			if isfile(join(_path,f)):
+				if os.path.splitext(f)[1]==_filetype:
+					print(join(_path,f))
+					# Then use xargs -I % cmd %
+		_oldpath = _path
+if __name__ == '__main__':
+	main(sys.argv)
