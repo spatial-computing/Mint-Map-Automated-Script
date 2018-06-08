b093a29bcd0b66660bc6f07fc38bcf6060922920
George <shiweihuang@Shiweis-MacBook-Pro.local>
2018-06-08 12:59:46 -0700

    some modification

--- python/macro_convert_json/readme.py
+++
@@ -1,6 +0,0 @@
-
-# you should import xml2json before running, either using pip install or add it to the package of pycharm is ok
-# this is the python script that can convert xml/qml file to json file
-# just simply run : python main.py inputfile_path outfile_path
-# the inputfile should be xml/qml
-# the outputfile is the txt file
