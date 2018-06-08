70aeb36aac5f002ce9294d531ade30693a069721
George <shiweihuang@Shiweis-MacBook-Pro.local>
2018-06-08 12:42:39 -0700

    this is a modification of the main.py

---
+++ python/macro_convert_json/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.6
+from xml2json import xml2json
+import optparse
+import json
+import sys
+
+def main(argv):
+    xmlstring = ""
+    options = None
+
+    options = optparse.Values({"pretty": False})
+
+    filename = sys.argv[1]
+    xmlstring = open(filename).read()
+
+    strip_ns = 1
+
+    json_string = xml2json.xml2json(xmlstring,options,strip_ns)
+    json_data = json.loads(json_string)
+
+    output = sys.argv[2]
+    with open(output, 'w') as f:
+      json.dump(json_data, f, ensure_ascii=False)
+
+if __name__ == '__main__':
+     main(sys.argv)
