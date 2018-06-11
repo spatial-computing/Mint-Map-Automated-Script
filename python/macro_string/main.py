63f8808137b35305976dd64098c63e78a5e934b8
Libo Liu <liboliu@usc.edu>
2018-06-11 12:40:34 -0700

    rebuilt database and collecting data

---
+++ python/macro_string/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys, os
+import re
+import random
+import math
+
+def main():
+	method = sys.argv[1]
+	string = sys.argv[2]
+
+	if method == 'layer_name_to_layer_id':
+		theType = sys.argv[3]
+		tileFormat = sys.argv[4]
+		output = "%s_%s_%s" % (string, theType, tileFormat)
+		print(output.lower().replace(' ', '_'))
+	elif method == 'lowercase':
+		print(string.lower())
+	elif method == 'underline':
+		print(string.replace(' ',  '_'))
+	elif method == 'gen_layer_name':
+		print('%s_%s' % (string.replace(' ', '_'), math.ceil(random.random() * 100000)))
+
+USAGE = '''
+USAGE:
+	main.py layer_name_to_layer_id [layer name] [type] [tile format]
+	main.py lowercase [string]
+	main.py underline [string]
+'''
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		print(USAGE, file = sys.stderr)
+		exit(0)
+	main()
---
+++ sql/sqlite_dump.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE `metadata`(`k` TEXT PRIMARY KEY, `v` TEXT);
+INSERT INTO metadata VALUES('border_features','{"type":"FeatureCollection","features":[{"type":"Feature","properties":{"stroke":"#000000","stroke-width":3,"stroke-opacity":1,"fill":"#555555","fill-opacity":0.3},"geometry":{"type":"Polygon","coordinates":[]}}]}');
+INSERT INTO metadata VALUES('config_file_location','');
+INSERT INTO metadata VALUES('mbtiles_location','');
+INSERT INTO metadata VALUES('metajson_location','');
+INSERT INTO metadata VALUES('port','');
+INSERT INTO metadata VALUES('server','');
+INSERT INTO metadata VALUES('tileurl','/{z}/{x}/{y}');
+CREATE TABLE `layer`(`id` INTEGER PRIMARY KEY, `layerid` TEXT, `type` TEXT, `tileformat` TEXT, `name` TEXT, `sourceLayer` TEXT, `original_id` INTEGER, `hasData` BOOLEAN, `hasTimeline` BOOLEAN, `maxzoom` INTEGER, `minzoom` INTEGER, `bounds` TEXT, `mbfilename` TEXT, `directory_format` TEXT, `starttime` TEXT, `endtime` TEXT, `json_filename` TEXT, `server` TEXT, `tileurl` TEXT, `styleType` TEXT, `legend_type` TEXT, `legend` TEXT, `valueArray` TEXT, `colormap` TEXT, `original_dataset_bounds` TEXT, `mapping` TEXT, `ckan_url` TEXT, `create_at` DATETIME DEFAULT current_timestamp, `modified_at` DATETIME DEFAULT current_timestamp);
+CREATE TABLE `original`(`id` INTEGER PRIMARY KEY , `dataset_name` TEXT, `filename` TEXT, `filepath` TEXT, `gdalinfo` TEXT, `related_json` TEXT, `create_at` DATETIME DEFAULT current_timestamp, `modified_at` DATETIME DEFAULT current_timestamp);
+COMMIT;
