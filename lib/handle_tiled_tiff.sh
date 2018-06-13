a606e48289ab3c5c9c63130e776a62a180445c04
System Administrator <root@Adams-MacBook-Pro-3.local>
2018-06-13 14:52:37 -0700

    changes for debugging

--- lib/handle_tiled_tiff.sh
+++ lib/handle_tiled_tiff.sh
@@ -20,24 +20,33 @@ source $MINTCAST_PATH/lib/handle_tiff.sh
 handle_tiled_tiff(){
 	# Parse arguments from mintcast.sh:
 	TILE_DIR=$DATASET_DIR #Directory containing tiles
+	echo "TILE DIR: $DATASET_DIR"
 	FILENAME=$DATASET_NAME #Desired filename (without extension) for output
+	echo "FILENAME: $DATASET_NAME"
 	LAYER_NAME=$LAYER_NAME #Layer name (displayed on map)
+	echo "LAYER: $LAYER_NAME"
 	QML_FILE=$QML_FILE #Will be passed from mintcast.sh (remove this later)
+	echo "QML_FIlE: $QML_FILE"
 
+	echo "MINTCAST_PATH: $MINTCAST_PATH"
 	# Hard-coded paths (passed from mintcast.sh?):
 	OUT_DIR="$MINTCAST_PATH/dist"
+	echo "OUT_DIR: $OUT_DIR"
 	if [[ ! -d "$OUT_DIR" ]]; then
 		mkdir -p "$OUT_DIR"
 	fi
-	if [[ $DEV_MODE != 'NO' ]]; then
+	if [[ $DEV_MODE != 'YES' ]]; then
+		echo "TARGET_MBTILES_PATH: $TARGET_MBTILES_PATH"
 		OUT_DIR=$TARGET_MBTILES_PATH
 	fi	#OUT_DIR=$MINTCAST_PATH/dist
 	TEMP_DIR=$OUT_DIR
+	echo "TEMP_DIR: $TEMP_DIR"
 
 	if [[ $WITH_QUALITY_ASSESSMENT != 'NO' ]]; then
 		FILE_EXT='num.tif'
 	else
 		FILE_EXT=$TILED_FILE_EXT
+		echo "FILE_EXT: $TILED_FILE_EXT"
 	fi
 
 
@@ -48,9 +57,11 @@ handle_tiled_tiff(){
 		CLEANED_TILE_DIR=$TILE_DIR
 	fi
 	CLEANED_FILENAME=${FILENAME%.*} #Remove extension from output filename
+	echo "CLEANED_FILENAME: $CLEANED_FILENAME"
 
 	# Set names for intermediate and output files:
 	MERGE_OUT=$TEMP_DIR/$CLEANED_FILENAME.tif #Merged tiles
+	echo "MERGE OUT: $MERGE_OUT"
 
 	# Check to see if data is zipped, unzip if necessary:
 	check_zipped $CLEANED_TILE_DIR $FILE_EXT
@@ -59,7 +70,8 @@ handle_tiled_tiff(){
 	gdal_merge.py \
 	-o $MERGE_OUT \
 	$CLEANED_TILE_DIR/*$FILE_EXT
-	INPUT=$MERGE_OUT
+	DATAFILE_PATH=$MERGE_OUT
+	echo "DATAFILE_PATH: $DATAFILE_PATH"
 
 	# Choose and execute routine (TIFF or TIFF w/ QML):
 	if [ "$QML_FILE" == "" ]; then
