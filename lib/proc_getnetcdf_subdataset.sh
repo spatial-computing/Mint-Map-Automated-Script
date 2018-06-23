6b8268d3e6641fba82d672ef2c3f02cb6aa1f25c
advaccaro <avaccaro@usc.edu>
2018-06-22 21:03:31 -0700

    weird bug in get nc subdataset

--- lib/proc_getnetcdf_subdataset.sh
+++ lib/proc_getnetcdf_subdataset.sh
@@ -5,14 +5,19 @@ proc_getnetcdf_subdataset(){
 	SUBDATASET_LAYAERS_ARRAY=()
 	NETCDF_FILEPATH=$1
 	SUBDATASET_STRING="$(gdalinfo $NETCDF_FILEPATH | sed -nE 's/SUBDATASET_.{1,2}_NAME=(.*)/\1/p' | grep -o 'N.*')"
-	IFS=$'\n'
-	SUBDATASETS=($SUBDATASET_STRING)
-	old=$IFS
+	
+	SUBDATASETS=($(echo "$SUBDATASET_STRING" | awk -F='\n' '{print $1}' ))
+	# SUBDATASETS=($(echo "$SUBDATASET_STRING" | tr ' ' '\n' ))
+	# SUBDATASETS=($SUBDATASET_STRING)
+	# SUBDATASETS=(`echo $SUBDATASET_STRING`)
+	# old=$IFS
 	for dataset in "${SUBDATASETS[@]}"; do
+		echo "${SUBDATASETS[1]}"
 		IFS=$':'
 		name=($dataset)
 		if [[ ${name[2]} != 'time_bnds' ]]; then
-			gdalwarp -t_srs EPSG:4326 "$dataset" "$MINTCAST_PATH/tmp/$DATASET_NAME.subset.${name[2]}.tif"
+			echo "gdalwarp -t_srs EPSG:4326 \"$dataset\" \"$MINTCAST_PATH/tmp/$DATASET_NAME.subset.${name[2]}.tif\""
+			# gdalwarp -t_srs EPSG:4326 "$dataset" "$MINTCAST_PATH/tmp/$DATASET_NAME.subset.${name[2]}.tif"
 			# gdalwarp -t_srs EPSG:3857 "$dataset" "$MINTCAST_PATH/tmp/$DATASET_NAME.subset.${name[2]}.tif"
 			SUBDATASETS_ARRAY+=("$MINTCAST_PATH/tmp/$DATASET_NAME.subset.${name[2]}.tif")
 			SUBDATASET_LAYAERS_ARRAY+=(${name[2]})
@@ -23,6 +28,5 @@ proc_getnetcdf_subdataset(){
 		# gdalwarp -te 22.4 3.4 37.0 23.2  -dstnodata 255 -cutline $MINTCAST_PATH/shp/ss.shp 
 		# exit 0
 	done
-	IFS=$old
 }
  
