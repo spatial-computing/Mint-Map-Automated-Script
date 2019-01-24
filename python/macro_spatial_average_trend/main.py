155a77e54f33d4ff020e2654f7e4d151947a4104
advaccaro <avaccaro@usc.edu>
2019-01-23 17:34:21 -0800

    Spatial average trends export to MongoDB

--- python/macro_spatial_average_trend/main.py
+++ python/macro_spatial_average_trend/main.py
@@ -7,74 +7,99 @@ import numpy as np
 from matplotlib import pyplot as plt
 from copy import copy
 import csv
-
 MINTCAST_PATH = os.environ.get('MINTCAST_PATH')
 dist_path = MINTCAST_PATH + '/dist/'
 temp_path = MINTCAST_PATH + '/tmp/'
+config_path = MINTCAST_PATH + "/config/"
+sys.path.append(config_path)
+from postgres_config import MONGODB_CONNECTION
+import pymongo
 
 # Calculate spatial average trend and export as csv
 
 class raster:
-    def __init__(self, filepath, var=""):
-        self.filepath = filepath
-        self.filename = os.path.basename(filepath)
-        self.dir = os.path.dirname(filepath)
-        self.ext = filepath.split('.')[-1]
-        if self.ext in ["tif", "tiff"]: 
-            raster = gdal.Open(filepath)
-        elif self.ext == "nc":
-            if var:
-                raster = gdal.Open('NETCDF:"' + filepath + '":' + var)
-            else: 
-                print("Need to specify variable.")
-        else:
-            print("Unknown raster type, check type and code.")
-        self.raster = raster
-        self.xsize = raster.RasterXSize
-        self.ysize = raster.RasterYSize
-        self.matrix = raster.ReadAsArray(xoff=0, yoff=0, xsize=self.xsize, ysize=self.ysize)
-        # Get Geographic meta data
-        self.geo_trans = raster.GetGeoTransform()
-        self.proj_str = raster.GetProjection()
-        self.num_bands = raster.RasterCount
-        self.cell_area = abs(self.geo_trans[1] * self.geo_trans[5])
-        self.ulx, self.xres, self.xskew, self.uly, self.yskew, self.yres = raster.GetGeoTransform()
-        self.lrx = self.ulx + self.xsize * self.xres 
-        self.lry = self.uly + self.ysize * self.yres
-        
-    def plot(self):
-        plt.imshow(self.matrix)
+	def __init__(self, filepath, var=""):
+		self.filepath = filepath
+		self.filename = os.path.basename(filepath)
+		self.dir = os.path.dirname(filepath)
+		self.ext = filepath.split('.')[-1]
+		if self.ext in ["tif", "tiff"]: 
+			raster = gdal.Open(filepath)
+		elif self.ext == "nc":
+			if var:
+				raster = gdal.Open('NETCDF:"' + filepath + '":' + var)
+			else: 
+				print("Need to specify variable.")
+		else:
+			print("Unknown raster type, check type and code.")
+		self.raster = raster
+		self.xsize = raster.RasterXSize
+		self.ysize = raster.RasterYSize
+		self.matrix = raster.ReadAsArray(xoff=0, yoff=0, xsize=self.xsize, ysize=self.ysize)
+		# Get Geographic meta data
+		self.geo_trans = raster.GetGeoTransform()
+		self.proj_str = raster.GetProjection()
+		self.num_bands = raster.RasterCount
+		self.cell_area = abs(self.geo_trans[1] * self.geo_trans[5])
+		self.ulx, self.xres, self.xskew, self.uly, self.yskew, self.yres = raster.GetGeoTransform()
+		self.lrx = self.ulx + self.xsize * self.xres 
+		self.lry = self.uly + self.ysize * self.yres
+		
+	def plot(self):
+		plt.imshow(self.matrix)
 
 def spatial_average_fldas(directory, var):
-    mon_dict = {}
-    for file in os.listdir(directory):
-        filepath = directory + file
-        opened_raster = raster(filepath, var)
-        day_str = str(int(file.split('.001')[0][-2:]))
-        no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
-        mon_dict[day_str] = np.mean(no_nans)
-        #print(jan_dict[day_str])
-    return(mon_dict)
+	mon_dict = {}
+	for file in os.listdir(directory):
+		filepath = directory + file
+		opened_raster = raster(filepath, var)
+		day_str = str(int(file.split('.001')[0][-2:]))
+		no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
+		mon_dict[day_str] = np.mean(no_nans)
+		#print(jan_dict[day_str])
+	return(mon_dict)
 
 
 # headers = ['Day Index', 'FLDAS', 'Chirps']
 def write_csv_timeseries(csv_outpath, headers, list_day_dicts):
 	with open(csv_outpath, mode='w') as fc_chart:
-	    fc_writer = csv.writer(fc_chart, delimiter=',', quotechar="'")
-	    fc_writer.writerow(headers)
-	    fd = list_day_dicts[0]
-	    for day in fd:
-	    	day_row = [day]
-	        #day = str(day)
-	        for day_dict in list_day_dicts:
-		        try:
-		            f_entry = '{:.12f}'.format(fd[day])
-		        except:
-		            f_entry = fd[day]
-    	        f_entry = '\"' + f_entry + '\"'
-		        day_row.append(f_entry)
-	        #print(jan_entry, feb_entry)
-	        fc_writer.writerow(day_row)
+		fc_writer = csv.writer(fc_chart, delimiter=',', quotechar="'")
+		fc_writer.writerow(headers)
+		fd = list_day_dicts[0]
+		for day in fd:
+			day_row = [day]
+			#day = str(day)
+			for day_dict in list_day_dicts:
+				try:
+					f_entry = '{:.12f}'.format(day_dict[day])
+				except:
+					f_entry = day_dict[day]
+				f_entry = '\"' + f_entry + '\"'
+				day_row.append(f_entry)
+			#print(jan_entry, feb_entry)
+			fc_writer.writerow(day_row)
+
+
+def write_trend_string(headers, list_day_dicts):
+	trend_string = ""
+	# Write headers
+	for idx, header in enumerate(headers):
+		if idx != len(headers)-1:
+			trend_string += (str(header)+ ",")
+		elif idx == len(headers)-1:
+			trend_string += (str(header)+ "\n")
+	# Write date rows
+	fd = list_day_dicts[0]
+	for day in fd:
+		day_str = str(day) + ","
+		for day_dict in list_day_dicts:
+			try:
+				f_entry = '{:.12f}'.format(day_dict[day])
+			except:
+				f_entry = day_dict[day]
+			day_str += '\"' + str(f_entry) + '\",'
+		trend_string += day_str[:-1] + "\n"
+	return(trend_string)
 
 def make_intersection(raster1, raster2):
 	# Find intersection
@@ -146,7 +171,18 @@ def parse_time(timestr, start_or_end):
 			daystr = '31'
 	return([yearstr, monstr, daystr])
 
-
+def export_to_mongodb(trend_string, trend_name, MONGODB_CONNECTION=MONGODB_CONNECTION):
+	mongo_client = pymongo.MongoClient(MONGODB_CONNECTION) # defaults to port 27017
+	mongo_db = mongo_client["mintcast"]
+	mongo_col = mongo_db["trend"]
+	ftmp = mongo_col.find_one({'relative_path': trend_name})
+	if ftmp:
+		mongo_col.update_one({'relative_path': trend_name}, { '$set': trend_string })
+	else:
+		mtmp = dict()
+		mtmp[trend_name] = trend_string
+		mongo_col.insert_one(mtmp)    
+		#mongo_col.insert_one({'relative_path': trend_name}, { '$set': trend_string })
 #predefine daily time axis
 #### combine the two
 year = str(2001)
@@ -182,32 +218,32 @@ md[oct_mon] = oct_days
 md[nov_mon] = nov_days
 md[dec_mon] = dec_days
 
-def fldas_chirps_demo(fldas_jan_path, fldas_feb_path, fldas_var, chirps_path, csv_outpath, headers):
+def fldas_chirps_demo(fldas_jan_path, fldas_feb_path, fldas_var, chirps_path, trend_name, headers):
 	fd = {}
 	cd = {}
 	for mon in md:
-	    for day in md[mon]:
-	        datestr = mon + "/" + day + "/" + year
-	        fd[datestr] = cd[datestr] = "null"
+		for day in md[mon]:
+			datestr = mon + "/" + day + "/" + year
+			fd[datestr] = cd[datestr] = "null"
 	#### FLDAS 01
 	mon_str = '1'
 	for file in os.listdir(fldas_jan_path):
-	    day_str = str(int(file.split('.001')[0][-2:]))
-	    date_str = mon_str + "/" + day_str + "/" + year
-	    filepath = jan_path + file
-	    opened_raster = raster(filepath, fldas_var)
-	    no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
-	    fd[date_str] = np.mean(no_nans) * 86400 * 30
+		day_str = str(int(file.split('.001')[0][-2:]))
+		date_str = mon_str + "/" + day_str + "/" + year
+		filepath = fldas_jan_path + file
+		opened_raster = raster(filepath, fldas_var)
+		no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
+		fd[date_str] = np.mean(no_nans) * 86400 * 30
 
 	#### FLDAS 02
 	mon_str = '2'
 	for file in os.listdir(fldas_feb_path):
-	    day_str = str(int(file.split('.001')[0][-2:]))
-	    date_str = mon_str + "/" + day_str + "/" + year
-	    filepath = feb_path + file
-	    opened_raster = raster(filepath, rainfall_var)
-	    no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
-	    fd[date_str] = np.mean(no_nans) * 86400 * 30
+		day_str = str(int(file.split('.001')[0][-2:]))
+		date_str = mon_str + "/" + day_str + "/" + year
+		filepath = fldas_feb_path + file
+		opened_raster = raster(filepath, fldas_var)
+		no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
+		fd[date_str] = np.mean(no_nans) * 86400 * 30
 
 	#### Chirps
 	files = [file for file in os.listdir(chirps_path) if file.endswith('clip.tif')]
@@ -215,20 +251,23 @@ def fldas_chirps_demo(fldas_jan_path, fldas_feb_path, fldas_var, chirps_path, cs
 	chirps_dict = {}
 	day_str = "15"
 	for file in files:
-	    filepath = chirps_path + file
-	    print(filepath)
-	    date_str = filepath.split('v1.8.')[1].split('_clip.tif')[0]
-	    mon_str = str(int(date_str[-2:]))
-	    year_str = date_str[:4]
-	    mon_year_str = mon_str + "/" + year_str
-	    date_str = mon_str + "/" + day_str + "/" + year_str
-	    opened_raster = raster(filepath)
-	    no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
-	    cd[date_str] = np.mean(no_nans)
+		filepath = chirps_path + file
+		print(filepath)
+		date_str = filepath.split('v1.8.')[1].split('_clip.tif')[0]
+		mon_str = str(int(date_str[-2:]))
+		year_str = date_str[:4]
+		mon_year_str = mon_str + "/" + year_str
+		date_str = mon_str + "/" + day_str + "/" + year_str
+		opened_raster = raster(filepath)
+		no_nans = opened_raster.matrix[opened_raster.matrix > -9999]
+		cd[date_str] = np.mean(no_nans)
 	
 	#### Write CSV file
 	list_day_dicts = [fd, cd] #combine dicts in list
-	write_csv_timeseries(csv_outpath, headers, list_day_dicts):
+	#write_csv_timeseries(csv_outpath, headers, list_day_dicts)
+	trend_string = write_trend_string(headers, list_day_dicts)
+	print(trend_string)
+	export_to_mongodb(trend_string, trend_name)
 
 def run_tiff2(tiff_tar1, tiff_tar2, headers, start_time, end_time, csv_outpath):
 	# Parse start and end times
@@ -239,7 +278,10 @@ def run_tiff2(tiff_tar1, tiff_tar2, headers, start_time, end_time, csv_outpath):
 	# Parse tiff_tar1 (single file, list of files, directory, or list of directories)
 	tiff1_list = parse_tiff_tar(tiff_tar1)
 	if len(tiff1_list) == 1:
+		print('Null')
+
 	elif len(tiff1_list) > 1:
+		print('Null')
 	else:
 		print("No TIFFs found in tiff_tar1", file = sys.stderr)
 		exit(0)
@@ -247,7 +289,9 @@ def run_tiff2(tiff_tar1, tiff_tar2, headers, start_time, end_time, csv_outpath):
 	# Parse tiff_tar2 (single file, list of files, directory, or list of directories)
 	tiff2_list = parse_tiff_tar(tiff_tar2)
 	if len(tiff2_list) == 1:
+		print('Null')
 	elif len(tiff2_list) > 1:
+		print('Null')
 	else:
 		print("No TIFFs found in tiff_tar2", file = sys.stderr)
 		exit(0)
@@ -270,9 +314,9 @@ def main():
 		fldas_feb_path = sys.argv[3] #".../data/South_Sudan/RawData/Forcing/FLDAS_NOAH01_A_EA_D/2001/02/"
 		chirps_path = sys.argv[4] #""
 		fldas_var = sys.argv[5] #"Rainf_f_tavg"
-		csv_outpath = sys.argv[6] #'./dist/fldas_chirps_2001_timeseries_chart.csv'
+		trend_name = sys.argv[6] #'./dist/fldas_chirps_2001_timeseries_chart.csv'
 		headers = sys.argv[7].split(',') #['Day Index', 'FLDAS', 'Chirps']
-		fldas_chirps_demo(fldas_jan_path, fldas_feb_path, fldas_var, chirps_path, csv_outpath, headers)
+		fldas_chirps_demo(fldas_jan_path, fldas_feb_path, fldas_var, chirps_path, trend_name, headers)
 	elif method == "tiff2":
 		'''Comparison between two TIFF files/directories/list'''
 		tiff_tar1 = sys.argv[2]
@@ -319,9 +363,6 @@ def main():
 		print(usage, file = sys.stderr)
 		exit(0)
 
-
-
-	elif method == ""
 if __name__ == '__main__':
 	num_args = len(sys.argv)
 	if num_args < 4:
